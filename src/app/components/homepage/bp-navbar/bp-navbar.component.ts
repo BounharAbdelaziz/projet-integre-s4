318cfd12df6acaed3254cb6c1bdc14a069c70253
MBbk101 <48057477+MBbk101@users.noreply.github.com>
2019-03-17 10:27:05 +0100

    Add files via upload

---
+++ src/app/components/homepage/bp-navbar/bp-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bp-navbar',
+  templateUrl: './bp-navbar.component.html',
+  styleUrls: ['./bp-navbar.component.css']
+})
+export class BpNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
